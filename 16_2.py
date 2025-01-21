95088c68ed4a7d1337a352c1099095a763d8d7e6
alexander <molokan2007@mail.ru>
2025-01-21 16:52:01 +0300

    Update 16_2.py

--- 16_2.py
+++ 16_2.py
@@ -14,10 +14,10 @@ async def mess() -> dict:
     return {"message" : "Главная страница"}
 
 @app.get("/user/{user_id}")
-async def user_id(user_id: int = Path(ge=1, le= 100, description= "Enter User ID" , example="13")) -> dict:
+async def user_id(user_id: Annotated[int,Path(ge=1, le= 100, description= "Enter User ID" , example="13")]) -> dict:
     return {"message" : f"Вы вошли как пользователь № {user_id}"}
 
 @app.get("/user/{username}/{age}")
-async def user_info(username: str = Path(min_length=5, max_length=20, description= "Enter Username", example= "Bender") ,
-                    age: int = Path(ge=18, le=120, description= "Enter Age" ,example= 30)) -> dict:
+async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description= "Enter Username", example= "Bender")] ,
+                    age: Annotated[int, Path(ge=18, le=120, description= "Enter Age" ,example= 30)]) -> dict:
     return {"message" : f'Информация о пользователе. Имя: {username} , Возраст: {age}'}
